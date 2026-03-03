7d8a828e7c735ae5d4fb29b2b7aa025113bccfb6
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-03 16:24:49 -0500

    added task properties and sample tasks

--- ProjectUIDesign/Task.swift
+++ ProjectUIDesign/Task.swift
@@ -5,3 +5,56 @@
 //  Created by Penny Ahlstrom on 2026-03-03.
 //
 
+import Foundation
+
+struct Task: Identifiable, Hashable {
+    let id = UUID()
+    // let courseID: UUID? // Tasks should be connected to a specific course
+    let title: String
+    let type: String // I made this string for now but should be an enum TaskType
+    //let dueDate: Date?
+    let dueDate: String // Making this string for now
+    let isCompleted: Bool
+    let isBonus: Bool
+    let isPriority: Bool
+    //let priorityThresholdDays: Int?
+    //let manualPriorityOverride: Bool?
+    let weight: Double
+    let scorePercent: Double
+    //let reminders: List<Reminders>?
+}
+
+extension Task {
+    static let sampleTasks: [Task] = [
+        .init(
+            title: "LabEx1",
+            type: "LAB",
+            dueDate: "2026-02-23",
+            isCompleted: true,
+            isBonus: false,
+            isPriority: false,
+            weight: 1.5,
+            scorePercent: 100
+        ),
+        .init(
+            title: "Assignment 1",
+            type: "ASSIGNMENT",
+            dueDate: "2026-02-23",
+            isCompleted: true,
+            isBonus: false,
+            isPriority: false,
+            weight: 15,
+            scorePercent: 100
+        ),
+        .init(
+            title: "Quiz Week 1",
+            type: "QUIZ",
+            dueDate: "2026-02-23",
+            isCompleted: false,
+            isBonus: false,
+            isPriority: true,
+            weight: 2,
+            scorePercent: 0
+        )
+    ]
+}
