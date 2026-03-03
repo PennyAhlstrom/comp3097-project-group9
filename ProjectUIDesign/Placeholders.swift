e6f3a9be540f68a05158eb642d3e6fd17b0be8b0
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-03 15:58:49 -0500

    added placeholders file for views not yet coded

---
+++ ProjectUIDesign/Placeholders.swift
@@ -0,0 +1,29 @@
+//
+//  Placeholders.swift
+//  ProjectUIDesign
+//
+//  Created by Penny Ahlstrom on 2026-03-03.
+//
+
+import SwiftUI
+
+struct TaskListPlaceholderView: View {
+    var body: some View {
+        Text("Task list coming next")
+            .navigationTitle("Tasks")
+    }
+}
+
+struct ReminderListPlaceholderView: View {
+    var body: some View {
+        Text("Reminder list coming next")
+            .navigationTitle("Reminders")
+    }
+}
+
+struct ProgressPlaceholderView: View {
+    var body: some View {
+        Text("Progress coming next")
+            .navigationTitle("Progress")
+    }
+}
