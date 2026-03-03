de5242111f681775914186ebe48a8939568758c1
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-03 16:36:08 -0500

    updated reminder list view

--- ProjectUIDesign/ReminderListView.swift
+++ ProjectUIDesign/ReminderListView.swift
@@ -5,3 +5,29 @@
 //  Created by Penny Ahlstrom on 2026-03-03.
 //
 
+import SwiftUI
+
+struct ReminderListView: View {
+    let reminders: [Reminder]
+
+    var body: some View {
+        List(reminders) { reminder in
+            VStack(alignment: .leading, spacing: 4) {
+                Text(reminder.message)
+                    .font(.headline)
+
+                Text("Scheduled: \(reminder.scheduledAt)")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            .padding(.vertical, 6)
+        }
+        .navigationTitle("Reminders")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ReminderListView(reminders: Reminder.sampleReminders)
+    }
+}
