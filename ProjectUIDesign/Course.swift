bb92dbd61485497a5d6560a6cb6efda887114736
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-03 14:17:27 -0500

    added course properties and sample courses

--- ProjectUIDesign/Course.swift
+++ ProjectUIDesign/Course.swift
@@ -5,3 +5,48 @@
 //  Created by Penny Ahlstrom on 2026-03-03.
 //
 
+import Foundation
+
+struct Course: Identifiable, Hashable {
+    let id = UUID()
+    let code: String
+    let title: String
+    let instructor: String
+    //let schedule: List<ScheduleItem>
+    let gradeGoal: Int?
+    let startWeek: Date?
+    //let tasks: List<Task>?
+}
+
+extension Course {
+    static let sampleCourses: [Course] = [
+        .init(
+            code: "COMP3097"
+            title: "Mobile Dev II",
+            instructor: "Professor Kiani",
+            gradeGoal: 85,
+            startWeek: Date()
+        ),
+        .init(
+            code: "COMP3132"
+            title: "Machine Learning",
+            instructor: "Professor Ajellu",
+            gradeGoal: 85,
+            startWeek: Date()
+        ),
+        .init(
+            code: "COMP3134"
+            title: "Cyber Security",
+            instructor: "Professor Blanc",
+            gradeGoal: 85,
+            startWeek: Date()
+        ),
+        .init(
+            code: "COMP3133"
+            title: "Fullstack Dev II",
+            instructor: "Professor Patel",
+            gradeGoal: 85,
+            startWeek: Date()
+        )
+    ]
+}
