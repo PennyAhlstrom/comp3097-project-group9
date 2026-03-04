c8e781fe05ff31649ff7abda28db7b8b8f3fc44b
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-03 23:07:11 -0500

    updated reminder detail view

--- ProjectUIDesign/ReminderDetailView.swift
+++ ProjectUIDesign/ReminderDetailView.swift
@@ -5,3 +5,22 @@
 //  Created by Penny Ahlstrom on 2026-03-03.
 //
 
+import SwiftUI
+
+struct ReminderDetailView: View {
+    let reminder: Reminder
+
+    var body: some View {
+        List {
+            Section("Reminder") {
+                DetailRow(label: "Message", value: reminder.message)
+                DetailRow(label: "Scheduled At", value: reminder.scheduledAt)
+            }
+        }
+        .navigationTitle("Reminder")
+    }
+}
+
+#Preview {
+    NavigationStack { ReminderDetailView(reminder: Reminder.sampleReminders[0]) }
+}
