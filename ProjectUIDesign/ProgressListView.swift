04cc24058b1c0089491e1fedca271ce550dde170
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-04 16:18:43 -0500

    updated progress list view navigation to use course id instead of progress id

--- ProjectUIDesign/ProgressListView.swift
+++ ProjectUIDesign/ProgressListView.swift
@@ -5,82 +5,6 @@
 //  Created by Penny Ahlstrom on 2026-03-03.
 //
 
-//import SwiftUI
-//
-//struct ProgressListView: View {
-//    let progresses: [Progress]
-//
-//    var body: some View {
-//        List(progresses) { progress in
-//            NavigationLink {
-//                    ProgressDetailView(progress: progress)
-//            } label: {
-//                VStack(alignment: .leading, spacing: 6) {
-//                    Text("Week Of: \(progress.weekOf)")
-//                        .font(.headline)
-//                    
-//                    if let grade = progress.currentGradePercent {
-//                        Text("Current Grade: \(grade, specifier: "%.0f")%")
-//                            .font(.subheadline)
-//                            .foregroundColor(.secondary)
-//                    }
-//                    
-//                    HStack(spacing: 12) {
-//                        if let acc = progress.accumulatedPercentPoints {
-//                            Text("Accum: \(acc, specifier: "%.0f")")
-//                                .font(.caption)
-//                                .foregroundColor(.secondary)
-//                        }
-//                        if let used = progress.usedPercentPoints {
-//                            Text("Used: \(used, specifier: "%.0f")")
-//                                .font(.caption)
-//                                .foregroundColor(.secondary)
-//                        }
-//                        if let lost = progress.lostPercentPoints {
-//                            Text("Lost: \(lost, specifier: "%.0f")")
-//                                .font(.caption)
-//                                .foregroundColor(.secondary)
-//                        }
-//                    }
-//                    
-//                    if let canMeet = progress.canMeetGoal {
-//                        Text(canMeet ? "On track to meet goal" : "At risk of missing goal")
-//                            .font(.caption)
-//                            .foregroundColor(.secondary)
-//                    }
-//                }
-//                .padding(.vertical, 6)
-//            }
-//        }
-//        .navigationTitle("Progress")
-//    }
-//}
-
-//import SwiftUI
-//
-//struct ProgressListView: View {
-//    @EnvironmentObject var store: AppStore
-//
-//    var body: some View {
-//        List(store.progresses) { progress in
-//            NavigationLink {
-//                ProgressDetailView(progressID: progress.id)
-//            } label: {
-//                VStack(alignment: .leading, spacing: 6) {
-//                    Text("Week Of: \(progress.weekOf)").font(.headline)
-//                    if let grade = progress.currentGradePercent {
-//                        Text("Current Grade: \(grade, specifier: "%.0f")%")
-//                            .font(.subheadline)
-//                            .foregroundColor(.secondary)
-//                    }
-//                }
-//                .padding(.vertical, 6)
-//            }
-//        }
-//        .navigationTitle("Progress")
-//    }
-//}
-
 import SwiftUI
 
 struct ProgressListView: View {
@@ -90,11 +14,11 @@ struct ProgressListView: View {
         List(store.courses) { course in
             let latest = latestProgress(for: course.id)
             
-            if let progress = latest {
+            if latest != nil {
                 NavigationLink {
-                    ProgressDetailView(progressID: progress.id)
+                    ProgressDetailView(courseID: course.id)
                 } label: {
-                    courseRow(course: course, progress: progress)
+                    courseRow(course: course, progress: latest)
                 }
             } else {
                 courseRow(course: course, progress: nil)
