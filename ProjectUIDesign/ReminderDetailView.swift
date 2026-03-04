534adca465c955d115ec6285a976b35d27311462
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-03 23:02:04 -0500

    added task and reminder detail views

---
+++ ProjectUIDesign/ReminderDetailView.swift
@@ -0,0 +1,7 @@
+//
+//  ReminderDetailView.swift
+//  ProjectUIDesign
+//
+//  Created by Penny Ahlstrom on 2026-03-03.
+//
+
---
+++ ProjectUIDesign/TaskDetailView.swift
@@ -0,0 +1,7 @@
+//
+//  TaskDetailView.swift
+//  ProjectUIDesign
+//
+//  Created by Penny Ahlstrom on 2026-03-03.
+//
+
