8fa4b3a10644a1756e686d644674a4bd678c79c0
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-04 11:52:31 -0500

    updated task model to link to a course using course id and use date type instead of string for due date

--- ProjectUIDesign/Task.swift
+++ ProjectUIDesign/Task.swift
@@ -9,24 +9,29 @@ import Foundation
 
 struct Task: Identifiable, Hashable {
     let id: UUID
-    // let courseID: UUID? // Tasks should be connected to a specific course - how do I do this
+    let courseID: UUID
+    
     let title: String
     let type: String // I made this string for now but should be an enum TaskType
-    let dueDate: String // Making this string for now
+    let dueDate: Date
+    
     let isCompleted: Bool
     let isBonus: Bool
     let isPriority: Bool
+    
     //let priorityThresholdDays: Int?
     //let manualPriorityOverride: Bool?
+    
     let weight: Double
     let scorePercent: Double
     //let reminders: List<Reminders>?
 
     init(
         id: UUID = UUID(),
+        courseID: UUID,
         title: String,
         type: String,
-        dueDate: String,
+        dueDate: Date,
         isCompleted: Bool,
         isBonus: Bool,
         isPriority: Bool,
@@ -34,6 +39,7 @@ struct Task: Identifiable, Hashable {
         scorePercent: Double
     ) {
         self.id = id
+        self.courseID = courseID
         self.title = title
         self.type = type
         self.dueDate = dueDate
@@ -45,37 +51,40 @@ struct Task: Identifiable, Hashable {
     }
 }
 
-extension Task {
-    static let sampleTasks: [Task] = [
-        .init(
-            title: "LabEx1",
-            type: "LAB",
-            dueDate: "2026-02-23",
-            isCompleted: true,
-            isBonus: false,
-            isPriority: false,
-            weight: 1.5,
-            scorePercent: 100
-        ),
-        .init(
-            title: "Assignment 1",
-            type: "ASSIGNMENT",
-            dueDate: "2026-02-23",
-            isCompleted: true,
-            isBonus: false,
-            isPriority: false,
-            weight: 15,
-            scorePercent: 100
-        ),
-        .init(
-            title: "Quiz Week 1",
-            type: "QUIZ",
-            dueDate: "2026-02-23",
-            isCompleted: false,
-            isBonus: false,
-            isPriority: true,
-            weight: 2,
-            scorePercent: 0
-        )
-    ]
-}
+//extension Task {
+//    static let sampleTasks: [Task] = [
+//        .init(
+//            courseID: ,
+//            title: "LabEx1",
+//            type: "LAB",
+//            dueDate: 2026-02-23,
+//            isCompleted: true,
+//            isBonus: false,
+//            isPriority: false,
+//            weight: 1.5,
+//            scorePercent: 100
+//        ),
+//        .init(
+//            courseID: ,
+//            title: "Assignment 1",
+//            type: "ASSIGNMENT",
+//            dueDate: "2026-02-23",
+//            isCompleted: true,
+//            isBonus: false,
+//            isPriority: false,
+//            weight: 15,
+//            scorePercent: 100
+//        ),
+//        .init(
+//            courseID: ,
+//            title: "Quiz Week 1",
+//            type: "QUIZ",
+//            dueDate: "2026-02-23",
+//            isCompleted: false,
+//            isBonus: false,
+//            isPriority: true,
+//            weight: 2,
+//            scorePercent: 0
+//        )
+//    ]
+//}
