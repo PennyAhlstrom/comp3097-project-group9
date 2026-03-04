ca9d96911c05fe672b3f162f348fc3a8b0542ded
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-04 14:09:39 -0500

    added course id to progress and updated week of to date instead of string type

--- ProjectUIDesign/Progress.swift
+++ ProjectUIDesign/Progress.swift
@@ -8,64 +8,94 @@
 import Foundation
 
 struct Progress: Identifiable, Hashable {
-    let id = UUID()
-    //let courseId: Int // I want this to connect to an existing course
-    let accumulatedPercentPoints: Double? // I want to initialize this to 0
-    let usedPercentPoints: Double? // I want to initialize this to 0
-    let lostPercentPoints: Double? // I want to initialize this to 0
-    let maxPossiblePercent: Double? // I want to initialize this to 100
-    let currentGradePercent: Double? // I want to initialize this to 0
-    let canMeetGoal: Bool? // I want to initialize this to T
-    let weekOf: String // String placeholder for now until I figure out how to set the date automatically
-    //let weekOf: Date?
-    //let computedAt: Date?
+    let id: UUID
+    let courseID: UUID
+    let accumulatedPercentPoints: Double
+    let usedPercentPoints: Double
+    let lostPercentPoints: Double
+    let maxPossiblePercent: Double
+    let currentGradePercent: Double
+    let canMeetGoal: Bool
+    let weekOf: Date
+    let computedAt: Date
+
+    init(
+        id: UUID = UUID(),
+        courseID: UUID,
+        accumulatedPercentPoints: Double,
+        usedPercentPoints: Double,
+        lostPercentPoints: Double,
+        maxPossiblePercent: Double,
+        currentGradePercent: Double,
+        canMeetGoal: Bool,
+        weekOf: Date,
+        computedAt: Date = Date()
+        
+    ) {
+        self.id = id
+        self.courseID = courseID
+        self.accumulatedPercentPoints = accumulatedPercentPoints
+        self.usedPercentPoints = usedPercentPoints
+        self.lostPercentPoints = lostPercentPoints
+        self.maxPossiblePercent = maxPossiblePercent
+        self.currentGradePercent = currentGradePercent
+        self.canMeetGoal = canMeetGoal
+        self.weekOf = weekOf
+        self.computedAt = computedAt
+    }
 }
 
 extension Progress {
+    static func date(_ string: String) -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.date(from: string)!
+    }
+    
     static let sampleProgresses: [Progress] = [
         .init(
+            courseID: Course.SampleIDs.course1,
             accumulatedPercentPoints: 12,
             usedPercentPoints: 15,
             lostPercentPoints: 3,
             maxPossiblePercent: 97,
             currentGradePercent: 80,
             canMeetGoal: true,
-            weekOf: "2026-01-12"
-            //weekOf: Date(), // This should be set as date of Monday for week it was calculated for
-            //computedAt: Date()
+            weekOf: date("2026-01-12"), // This should be set as date of Monday for week it was calculated for
+            computedAt: Date()
         ),
         .init(
+            courseID: Course.SampleIDs.course1,
             accumulatedPercentPoints: 22,
             usedPercentPoints: 25,
             lostPercentPoints: 3,
             maxPossiblePercent: 97,
             currentGradePercent: 88,
             canMeetGoal: true,
-            weekOf: "2026-01-26"
-            //weekOf: Date(), // This should be set as date of Monday for week it was calculated for
-            //computedAt: Date()
+            weekOf: date("2026-01-26"), // This should be set as date of Monday for week it was calculated for
+            computedAt: Date()
         ),
         .init(
+            courseID: Course.SampleIDs.course1,
             accumulatedPercentPoints: 39,
             usedPercentPoints: 45,
             lostPercentPoints: 6,
             maxPossiblePercent: 94,
             currentGradePercent: 87,
             canMeetGoal: true,
-            weekOf: "2026-02-09"
-            //weekOf: Date(), // This should be set as date of Monday for week it was calculated for
-            //computedAt: Date()
+            weekOf: date("2026-02-09"), // This should be set as date of Monday for week it was calculated for
+            computedAt: Date()
         ),
         .init(
+            courseID: Course.SampleIDs.course1,
             accumulatedPercentPoints: 58,
             usedPercentPoints: 65,
             lostPercentPoints: 7,
             maxPossiblePercent: 93,
             currentGradePercent: 89,
             canMeetGoal: true,
-            weekOf: "2026-02-23"
-            //weekOf: Date(), // This should be set as date of Monday for week it was calculated for
-            //computedAt: Date()
+            weekOf: date("2026-02-23"), // This should be set as date of Monday for week it was calculated for
+            computedAt: Date()
         )
     ]
 }
