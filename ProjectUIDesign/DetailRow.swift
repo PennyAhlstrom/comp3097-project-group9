0d53001de631e992301534ce80b01f7fe8fcb1c0
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-03 22:58:16 -0500

    updated detail row component

--- ProjectUIDesign/DetailRow.swift
+++ ProjectUIDesign/DetailRow.swift
@@ -5,3 +5,19 @@
 //  Created by Penny Ahlstrom on 2026-03-03.
 //
 
+import SwiftUI
+
+struct DetailRow: View {
+    let label: String
+    let value: String
+
+    var body: some View {
+        HStack(alignment: .top) {
+            Text(label)
+                .foregroundColor(.secondary)
+            Spacer()
+            Text(value)
+                .multilineTextAlignment(.trailing)
+        }
+    }
+}
