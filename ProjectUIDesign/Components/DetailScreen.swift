0d69813abdb1e08cf643dfe63abe352c1cb7ffb8
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-08 17:22:27 -0400

    added detail screen

---
+++ ProjectUIDesign/Components/DetailScreen.swift
@@ -0,0 +1,25 @@
+//
+//  DetailScreen.swift
+//  ProjectUIDesign
+//
+//  Created by Penny Ahlstrom on 2026-03-08.
+//
+
+import SwiftUI
+
+struct DetailScreen<Content: View>: View {
+    let background: Color
+    @ViewBuilder let content: () -> Content
+
+    var body: some View {
+        ZStack {
+            background
+                .ignoresSafeArea()
+
+            List {
+                content()
+            }
+            .scrollContentBackground(.hidden)
+        }
+    }
+}
