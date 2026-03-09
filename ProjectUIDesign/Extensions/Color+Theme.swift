30f795cc9ac90ee9fb55335ca6ee897334ea4577
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-03-08 21:14:55 -0400

    add home background color to color theme

--- ProjectUIDesign/Extensions/Color+Theme.swift
+++ ProjectUIDesign/Extensions/Color+Theme.swift
@@ -8,18 +8,21 @@
 import SwiftUI
 
 extension Color {
+    
+    static let homeBackground = Color.gray.opacity(0.1)
 
     static let coursesBackground = Color.blue.opacity(0.1)
     static let tasksBackground = Color.green.opacity(0.1)
-    static let progressBackground = Color.orange.opacity(0.1)
     static let remindersBackground = Color.purple.opacity(0.1)
+    static let progressBackground = Color.orange.opacity(0.1)
     
     static let coursesCurrentBar = Color.blue
     
     static let tasksCurrentBar = Color.green
     
+    static let remindersCurrentBar = Color.purple
+    
     static let progressCurrentBar = Color.orange
     static let progressPossibleBar = Color.orange.opacity(0.35)
     
-    static let remindersCurrentBar = Color.purple
 }
